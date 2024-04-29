0b428b1be851fc34fa66cfc24d654a9d73e8cc78
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-04-29 11:05:18 +0300

    add MovieController

---
+++ src/main/java/dev/nastiausenko/movies/MovieController.java
@@ -0,0 +1,30 @@
+package dev.nastiausenko.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/V1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> allMovies() {
+        return new ResponseEntity<>(service.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> findMovieById(@PathVariable String imdbId) {
+        return new ResponseEntity<>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/dev/nastiausenko/movies/MovieService.java
@@ -0,0 +1,23 @@
+package dev.nastiausenko.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> allMovies() {
+        return repository.findAll();
+    }
+
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findByImdbId(imdbId);
+    }
+}
